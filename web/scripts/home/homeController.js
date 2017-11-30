29d5adaedcd63637b65397bb05f5e431efb8c9e7
ssirinte <ssirinte@andrew.cmu.edu>
2017-11-30 13:29:33 -0500

    Very basic sessionStorage shopping cart which stores 1 item, and refreshes when you go to the homepage

--- web/scripts/home/homeController.js
+++ web/scripts/home/homeController.js
@@ -11,6 +11,12 @@ module.exports = {
     function registerRoutes (app) {
       router('/', () => {
         app.currentView = 'home'
+        sessionStorage.setItem("totalPrice", 0)
+        sessionStorage.setItem("productsInCart", [])
+        var productsInCart = sessionStorage.getItem("productsInCart")
+        var totalPrice = sessionStorage.getItem("totalPrice")
+        console.log("Products in Cart Currently: " + productsInCart.toString())
+        console.log("Total Price of Items in Cart: " + totalPrice.toString())
       })
     }
 
--- web/scripts/products/Product.js
+++ web/scripts/products/Product.js
@@ -44,8 +44,29 @@ module.exports = {
 
       self.addToCart = (event) => {
         console.log(`TODO: add ${self.title} to shopping cart`)
+        var product = self;
+        var productName = product.title
+        var productPrice = product.price
+        var quantity = 1
+        var currItems = sessionStorage.getItem("productsInCart")
+        var productsInCart = []
+        console.log(productsInCart)
+        productsInCart.push(productName)
+        var totalPrice = sessionStorage.getItem("totalPrice")
+        var price = 10.69
+        totalPrice = 0.00 + (productPrice * quantity)
+        sessionStorage.setItem("productsInCart", productsInCart);
+        sessionStorage.setItem("totalPrice", totalPrice);
+        console.log(`Product Name To Add: ${self.title}`)
+        console.log(`Product Quantity To Add: 1`)
+        console.log(`Product Price To Add: ${self.price}`)
+        console.log("Added to cart! Cart now contains: " + sessionStorage.getItem("productsInCart").toString())
       }
 
+      //var allTotals = sessionStorage.getItem("totalPrice")
+      //var allProducts = sessionStorage.getItem("productsInCart")
+      //console.log("The total price to date is: " + str(allTotals))
+      //console.log("The total items in the cart to date is: " + str(allProducts))
       return self
     }
   }
