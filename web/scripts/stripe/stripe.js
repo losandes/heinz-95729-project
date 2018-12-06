70520d7d6d006ef2e08891440871c6c622fe32e0
sushmits <sushmits@andrew.cmu.edu>
2018-12-05 22:28:04 -0500

    Made the changes recommended by Edwin in peer review

--- web/scripts/stripe/stripe.js
+++ web/scripts/stripe/stripe.js
@@ -1,31 +1,11 @@
-/*
-sk key : sk_test_tjoHHaJQvf1Y4CoAEpWBWr0w
-publish key :pk_test_4VicAuG5Ou9zuqA4LVIRI0dC
- from stripe.com
-
-*/
 // Refered to the video https://www.youtube.com/watch?v=FhBtM70QZKM
 // https://stripe.com/docs/stripe-js/elements/quickstart
-// npm install stripe
-// installing body parser
-// $ npm install body-parser
-// API
-
-// npm install express
-
-// install hbs
-// npm install hbs
-
 var express = require('express')
 var stripe = require('stripe')('sk_test_tjoHHaJQvf1Y4CoAEpWBWr0w')
 var bodyParser = require('body-parser')
 var hbs = require('hbs')
 
 var app = express()
-
-app.set('view engine', 'hbs')
-// eslint-disable-next-line no-path-concat
-app.set('views', __dirname + '/views')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -51,10 +31,6 @@ app.post('/charge', function (req, res) {
   console.log('Your payment was successful!')
   res.redirect('/paysuccess')
 })
-
-// app.listen(3000, function () {
-//   console.log('Stripe is running')
-// })
 
 
 
