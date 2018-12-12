75fb81ca3c18e8327362b351994216334ae288e2
sushmits <sushmits@andrew.cmu.edu>
2018-12-12 13:44:19 -0500

    made changes to homeComponent and homeController to display recommendations for user

--- web/scripts/home/homeComponent.js
+++ web/scripts/home/homeComponent.js
@@ -1,24 +1,42 @@
 module.exports = {
   scope: 'heinz',
   name: 'homeComponent',
-  dependencies: ['Vue', 'locale'],
-  factory: (Vue, locale) => {
+  dependencies: ['Vue', 'locale', 'productsComponent'],
+  factory: (Vue, locale, productsComponent) => {
     'use strict'
+    var state = { products: [] }
 
     const component = Vue.component('home', {
       template: `
-        <div class="component empty-component">
+        <!--div class="component empty-component">
           <h1>{{heading}}</h1>
-          <div>{{body}}</div>
+          <div>{{body}}</div-->
+        </div>
+        <div class="product-component details">
+          <figure class='product'>
+            <img :src="thumbnailLink" :alt="thumbnailAlt">
+            <figcaption>
+              <h1>{{title}}</h1>
+              <div>{{description}}</div>
+            </figcaption>
+          </figure>
+          <div class="purchase">
+            <button class="btn btn-success btn-buy" v-on:click="addToCart">{{price}}</button>
+          </div>
         </div>`,
       data: () => {
         return {
           heading: locale.pages.home.heading,
-          body: locale.pages.home.body
+          body: locale.pages.home.body,
+          state
         }
       }
     })
 
-    return { component }
+    const setProducts = (searchResults) => {
+      state = searchResults
+    }
+
+    return { component, setProducts }
   }
 }
--- web/scripts/home/homeController.js
+++ web/scripts/home/homeController.js
@@ -1,17 +1,51 @@
 module.exports = {
   scope: 'heinz',
   name: 'homeController',
-  dependencies: ['router'],
-  factory: (router) => {
+  dependencies: ['router', 'productsComponent', 'Product', 'productsRepo'],
+  factory: (router, productsCategoriesComponent, Product, repo) => {
     'use strict'
 
     /**
      * Route binding (controller)
      */
     function registerRoutes (app) {
+      // router('/', () => {
+      //   app.currentView = 'home'
+      // })
+
       router('/', () => {
-        app.currentView = 'home'
+        // let user = storage.get('user')
+        repo.search('fiction', (err, products) => {
+          if (err) {
+            console.log(err)
+            // TODO: render error view
+          }
+
+          if (products && products.length) {
+            productsCategoriesComponent.setProducts(new ProductSearchResult(products))
+            app.currentView = 'products'
+          } else {
+            // TODO: route to a "none found" page
+            router.navigate('/')
+          }
+        })
       })
+    }
+
+    /**
+     * A view model for the search results (an array of products)
+     * @param {Array} products - the products that were returned by the result
+     */
+    function ProductSearchResult (products) {
+      if (!Array.isArray(products)) {
+        return {
+          products: []
+        }
+      }
+
+      return {
+        products: products.map(product => new Product(product))
+      }
     }
 
     return { registerRoutes }
