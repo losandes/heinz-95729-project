2608c20514d52f56e1339a1bc1e93eb42c755d44
wynwinz <edm34@pitt.edu>
2018-12-13 16:12:20 -0500

    Categories are now a list to choose from

--- web/scripts/home/homeController.js
+++ web/scripts/home/homeController.js
@@ -6,37 +6,6 @@ module.exports = {
     'use strict'
 
     /**
-     * Route binding (controller)
-     */
-    function registerRoutes (app) {
-      router('/', () => {
-        const user = storage.get('user')
-        if (user !== null && user.categories[0] !== undefined) {
-          const chosenCategory = user.categories[0]
-          repo.search(chosenCategory, (err, products) => {
-            if (err) {
-              console.log(err)
-              // TODO: render error view
-            }
-
-            app.currentView = 'loading'
-            if (products && products.length) {
-              homeComponent.setProducts(new ProductSearchResult(products, chosenCategory))
-            } else {
-              // TODO: route to a "none found" page
-              homeComponent.setProducts(new ProductSearchResult(null))
-            }
-            app.currentView = 'home'
-          })
-        } else {
-          app.currentView = 'loading'
-          homeComponent.setProducts(new ProductSearchResult(null))
-          app.currentView = 'home'
-        }
-      })
-    }
-
-    /**
      * A view model for the search results (an array of products)
      * @param {Array} products - the products that were returned by the result
      */
@@ -49,10 +18,40 @@ module.exports = {
 
       return {
         products: products.map(product => new Product(product)),
-        category
+        category: capitalizeFirstLetter(category)
       }
     }
 
+    function capitalizeFirstLetter (string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    /**
+     * Route binding (controller)
+     */
+    function registerRoutes (app) {
+      router('/', () => {
+        const user = storage.get('user')
+        // If user is logged in, and they have at least 1 favorite category.
+        if (user !== null && user.categories[0] !== undefined) {
+          const chosenCategory = user.categories[0]
+          repo.search(chosenCategory, (err, products) => {
+            if (err) {
+              console.log(err)
+              // TODO: render error view
+            }
+            app.currentView = 'loading'
+            homeComponent.setProducts(new ProductSearchResult(products, chosenCategory))
+            app.currentView = 'home'
+          })
+        } else {
+          // Handle case of user not being logged in or having no favorites.
+          app.currentView = 'loading'
+          homeComponent.setProducts(new ProductSearchResult(null))
+          app.currentView = 'home'
+        }
+      })
+    }
     return { registerRoutes }
   }
 }
