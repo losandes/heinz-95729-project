c6830ce1e39c12f05d22114e1d19d90242702f6b
losandes <andes.collab@gmail.com>
2019-11-09 13:43:01 -0500

    Updates CUI dependencies

--- cui/Bot.js
+++ cui/Bot.js
@@ -2,7 +2,7 @@ module.exports = function Bot (Adventure) {
   const bot = new Adventure()
 
   bot.question('hello world!')
-    .then(answer => {
+    .then((answer) => {
       console.log(`You said, ${answer}`)
       console.log()
       console.log('Checkout `https://github.com/losandes/terminal-adventure` for more information on how to use Terminal Adventure')
