64f399d62165cfbb6641c496a2cdb8001186b55d
Peter Yefi <peteryef@andrew.cmu.edu>
2019-11-23 20:32:08 -0500

    Added .js extension to getBooks action

--- api/books/index.js
+++ api/books/index.js
@@ -1,6 +1,6 @@
 module.exports = [
   require('./actions/getBook.js'),
-  require('./actions/getBooks'),
+  require('./actions/getBooks.js'),
   require('./actions/searchBooks.js'),
   require('./Book.js'),
   require('./booksController.js')
