53144e11b08d1b763ea254f97dac1c560293186d
andrew/tjain <tanvijain1024@gmail.com>
2019-11-25 21:20:37 -0500

    Comments added

--- api/src/main/java/com/ecom/backrow/api/ApiApplication.java
+++ api/src/main/java/com/ecom/backrow/api/ApiApplication.java
@@ -3,6 +3,9 @@ package com.ecom.backrow.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Main class
+ */
 @SpringBootApplication
 public class ApiApplication {
 
