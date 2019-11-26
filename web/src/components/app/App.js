a7450a181cd4fa61ea3747fc266b6c7ba3e84c43
Salild1011 <salild1011@gmail.com>
2019-11-26 15:08:09 -0500

    Added FontAwesome icons

--- web/src/components/app/App.js
+++ web/src/components/app/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Header from './header/Header';
 
 function App() {
+  // Add fontAwesome icons to the library
+  library.add(faSearch)
+
   return (
     <div className="App">
       <Header>
