a95e69daf19cf04245b636f379dc254964071916
andrew/tjain <tjain@andrew.cmu.edu>
2019-11-25 20:43:28 -0500

    Deleted the old api project and created api project in spring
    Signed-off-by: andrew/tjain <tjain@andrew.cmu.edu>

---
+++ api/src/test/java/com/ecom/backrow/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.backrow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
