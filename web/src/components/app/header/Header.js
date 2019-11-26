d20df3bb05cdef70e0122ddbbed760be43fbb518
Salild1011 <salild1011@gmail.com>
2019-11-26 15:08:45 -0500

    Search bar for main page

--- web/src/components/app/header/Header.js
+++ web/src/components/app/header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import Jumbotron from "react-bootstrap/Jumbotron";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const header = (props) => {
@@ -8,12 +9,16 @@ const header = (props) => {
   return (
     <Jumbotron className="home-banner">
       <div className="filter">
-        <h1 className="welcome-title">Welcome to Backrow Shop</h1>
-        <h3 className="welcome-subtitle">Find the best products from the comfort of your home</h3>
+        <div className="banner-content">
+          <h1 className="welcome-title">Welcome to Backrow Shop</h1>
+          <h3 className="welcome-subtitle">Find the best products from the comfort of your home</h3>
 
-        <form>
-          <input type="text" />
-        </form>
+          <div className="search-container">
+            <input id="search-bar" type="text" onChange={props.inputListener} value={props.search} />
+            <button><FontAwesomeIcon icon="search" /></button>
+          </div>
+          
+        </div>
       </div>
     </Jumbotron>
   );
