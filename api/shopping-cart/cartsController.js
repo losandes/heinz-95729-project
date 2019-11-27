f7ee68ddb43befc097ad7a8d74470c36e0259fa9
Peter Yefi <peteryef@andrew.cmu.edu>
2019-11-27 03:03:33 -0500

    completed the implementation of adding an item to cart where items could be added to an existing cart after creating it

--- api/shopping-cart/cartsController.js
+++ api/shopping-cart/cartsController.js
@@ -1,9 +1,10 @@
 module.exports.name = 'cartsController'
-module.exports.dependencies = ['router', 'addToCart', 'logger']
-module.exports.factory = (router, _addToCart, logger) => {
+module.exports.dependencies = ['router', 'addToCart', 'addToExistingCart', 'logger']
+module.exports.factory = (router, _addToCart, _addToExistingCart, logger) => {
   'use strict'
 
-  const { getCart, addToCart, validateCart, createCart } = _addToCart
+  const { getCart, addToCart, validateCart, createCart, bindToCart } = _addToCart
+  const { addToExistingCart, updateCartTotal, bindUpdateToCart } = _addToExistingCart
   
   router.post('/carts/add', function (req, res) {
     const body = req.body
@@ -12,16 +13,36 @@ module.exports.factory = (router, _addToCart, logger) => {
       .then(body => new Promise(getCart(body.uid)))
       .then(cart => {
           if(cart == null){
-            //add new item to shopping cart
+            //create new cart and add item
             return Promise.resolve(createCart(body))
             .then(cart => new Promise(addToCart(cart)))
+            .then(cart => new Promise(bindToCart(cart)))
           }
           else{
-            //update the quantity of an exiting item in shopping cart
+            //add item to an existing cart
+            return new Promise(addToExistingCart(body, cart))
+            .then(cart => new Promise(bindUpdateToCart(cart)))
+            .then(cart => new Promise(updateCartTotal(cart, body)))
+            .then(cart => new Promise(bindUpdateToCart(cart)))
           }
       })
+      .then(() => new Promise(getCart(body.uid)))
       .then(cart => {
-        res.status(201).send(cart.ops[0])
+        res.status(201).send(cart)
+      })
+      .catch(err => {
+        logger.error(err)
+        res.status(400).send({ messages: [err.message] })
+      })
+  })
+
+
+  router.post('/carts/update-quantity', function (req, res) {
+    const body = req.body
+    Promise.resolve(body)
+      .then(body => new Promise(getCart(body.uid)))
+      .then(cart => {
+        res.status(201).send(cart)
       })
       .catch(err => {
         logger.error(err)
