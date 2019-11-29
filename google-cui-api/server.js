605740143e5b23720b66f8b4de054ec40e49f737
nicholeemma <790956043@qq.com>
2019-11-28 22:48:33 -0500

    separate cart and product

--- google-cui-api/server.js
+++ google-cui-api/server.js
@@ -2,7 +2,8 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Product = require('./api/models/productModel'), //created model loading here
+  Product = require('./api/models/productModel'),
+  Cart = require('./api/models/cartModel'), //created model loading here
   bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
