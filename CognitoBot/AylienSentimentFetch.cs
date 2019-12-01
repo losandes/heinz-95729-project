ad9b70057c65f7e98f6ed2b3e8d58f87a6a7367d
Keshav Pandey <keshav25@outlook.com>
2019-12-01 14:32:57 -0500

    Make code more testable

--- CognitoBot/AylienSentimentFetch.cs
+++ CognitoBot/AylienSentimentFetch.cs
@@ -12,12 +12,26 @@ namespace CognitoBot
         {
             var client = new RestClient("https://api.aylien.com/api/v1/sentiment?mode=tweet&text=" + text);
             var request = new RestRequest(Method.GET);
+            addHeaders(request);
+            IRestResponse response = client.Execute(request);
+            SentimentResponse reply = SimpleJson.DeserializeObject<SentimentResponse>(response.Content);
+            return reply.polarity;
+        }
+
+        public void addHeaders(RestRequest request) {
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("X-AYLIEN-TextAPI-Application-ID", "9915cb86");
             request.AddHeader("X-AYLIEN-TextAPI-Application-Key", "b0cdea5fc81ec683cb681eec282fcb12");
-            IRestResponse response = client.Execute(request);
-            return response.Content;
         }
+        public class SentimentResponse
+        {
+            public string polarity { get; set; }
+            public string subjectivity { get; set; }
+            public string text { get; set; }
+            public float polarity_confidence { get; set; }
+            public float subjectivity_confidence { get; set; }
+        }
+
     }
 }
