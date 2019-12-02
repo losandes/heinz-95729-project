3613abaab5bedc8ab3bb23b16d42f8e5346c84f7
Monika Jengaria <mjengari@andrew.cmu.edu>
2019-12-01 21:30:39 -0500

    Commit slack authentication code

--- CognitoBot/Controllers/CognitoController.cs
+++ CognitoBot/Controllers/CognitoController.cs
@@ -21,9 +21,48 @@ namespace CognitoBot.Controllers
 
         // POST: api/Default
         [HttpPost]
-        public string Post([FromBody] string value)
+        public HttpResponse Post([FromBody] string value)
         {
-            return "Hello Post world";
+            JObject json = JObject.Parse(value);
+            return json.SelectToken("challenge");
+
+            /*JObject json = JObject.Parse(value);
+            String text = json.SelectToken("event.text");
+            AylienSentimentFetch getSentiment = new AylienSentimentFetch();
+            String sentiment = getSentiment.getSentimentScore(text);
+            if(sentiment.Equals("positive")){
+                sentiment = "happy minion";
+            }else{
+                sentiment = "sad minion";
+            }
+            String giphyUrl =  getGiphy(sentiment);
+
+            createResponse();*/
+        }
+
+        public String getGiphy(String searchText){
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + searchText);
+            //request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                String response = reader.ReadToEnd();
+                JObject json = JObject.Parse(response);
+                String giphyUrl = json.SelectToken("data[0].url");
+                return giphyUrl;
+            }
+        }
+
+        public String createResponse(){
+            JObject json = new JObject();
+            
+            //{
+ // "channel": "YOUR_CHANNEL_ID",
+//  "text": "Hello, world"
+//}
         }
     }
 }
