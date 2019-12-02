c7cc132dc620494e196dcd324924e5185a569d37
Keshav Pandey <keshav25@outlook.com>
2019-12-01 22:28:04 -0500

    Add tests

---
+++ CognitoTestProject/AylienSentimentFetchTest.cs
@@ -0,0 +1,33 @@
+using CognitoBot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace CognitoTestProject
+{
+    [TestClass]
+    public class AylienSentimentFetchTest
+    {
+        [TestMethod]
+        public void TestHappySentiment()
+        {
+            AylienSentimentFetch sentimentFetch = new AylienSentimentFetch();
+            Assert.AreEqual("positive", sentimentFetch.getSentimentScore("I am Happy Today"));
+        }
+
+        [TestMethod]
+        public void TestSadSentiment()
+        {
+            AylienSentimentFetch sentimentFetch = new AylienSentimentFetch();
+            Assert.AreEqual("negative", sentimentFetch.getSentimentScore("I am Sad Today"));
+        }
+
+        [TestMethod]
+        public void TestHeaders()
+        {
+            AylienSentimentFetch sentimentFetch = new AylienSentimentFetch();
+            var request = new RestRequest(Method.GET);
+            sentimentFetch.addHeaders(request);
+            Assert.AreEqual(4, request.Parameters.Count);
+        }
+    }
+}
