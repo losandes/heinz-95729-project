60b50fdb89dbdf89ab56a8526dc4410fd07bbedb
Mengyue <42802258+menyw@users.noreply.github.com>
2019-12-03 20:27:38 -0500

    fix shopping cart page value

--- web/scripts/cart/cartComponent.js
+++ web/scripts/cart/cartComponent.js
@@ -15,7 +15,6 @@ module.exports = {
       // shopping cart UI code from:https://bootsnipp.com/snippets/yP7qe, by asanti82
       template: `
       <div class="container" >
-      <div>{{total}}</div>
         <table id="cart" class="table table-hover table-condensed">
                 <thead>
                 <tr>
@@ -27,21 +26,19 @@ module.exports = {
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                <tr v-for="item in items">
                   <td data-th="Product">
                     <div class="row">
-                      <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
                       <div class="col-sm-10">
-                        <h4 class="nomargin">Product 1</h4>
-                        <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+                        <h4 class="nomargin">{{item.name}}</h4>
                       </div>
                     </div>
                   </td>
-                  <td data-th="Price">$10</td>
+                  <td data-th="Price">{{item.price}}</td>
                   <td data-th="Quantity">
-                    <input type="number" class="form-control text-center" value="1">
+                    <input type="number" class="form-control text-center" :value="item.quantity">
                   </td>
-                  <td data-th="Subtotal" class="text-center">1.99</td>
+                  <td data-th="Subtotal" class="text-center">{{item.quantity * item.price}}</td>
                   <td class="actions" data-th="">
                     <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
                     <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
@@ -50,12 +47,12 @@ module.exports = {
               </tbody>
               <tfoot>
                 <tr class="visible-xs">
-                  <td class="text-center"<strong>Total 1.99</strong></td>
+                  <td class="text-center"<strong>Total {{total}}</strong></td>
                 </tr>
                 <tr>
                   <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
                   <td colspan="2" class="hidden-xs"></td>
-                  <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+                  <td class="hidden-xs text-center"><strong>Total $ {{total}}</strong></td>
                   <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
                 </tr>
               </tfoot>
