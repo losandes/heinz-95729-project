e6c49884dccd24e93a8b52f0b2743410499ce4c0
Peter Yefi <peteryef@andrew.cmu.edu>
2019-12-04 20:29:18 -0500

    Modified addOrder to set the amount of downloads available to a user

--- api/orders/actions/addOrder.js
+++ api/orders/actions/addOrder.js
@@ -4,7 +4,15 @@ module.exports.factory = function (repo, cartsRepo, Order) {
   'use strict'
 
   const addOrder = (cart) => (resolve, reject) => {
-    return repo.add(cart)
+    var order = Object.assign({}, cart)
+    var items = order.items
+   
+    for (var i=0; i < items.length; i++){
+      items[i].downloads = items[i].quantity
+    }
+    
+    order.items = items
+    return repo.add(order)
       .then(resolve)
       .catch(reject)
   }
