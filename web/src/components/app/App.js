48ff4291c8e52eaaece5d29bfff32d0a59bf7c4f
Salild1011 <salild1011@gmail.com>
2019-12-07 20:18:43 -0500

    Navbar and header placed and aligned on the main page

--- web/src/components/app/App.js
+++ web/src/components/app/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Header from './header/Header';
+import Navbar from '../nav/Navbar';
 
 function App() {
-  // Add fontAwesome icons to the library
-  library.add(faSearch)
+    // Add fontAwesome icons to the library
+    library.add(faSearch)
 
-  return (
-    <div className="App">
-      <Header>
-
-      </Header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className="navbar-container">
+                <Navbar />
+            </div>
+        </div>
+    );
 }
 
 export default App;
