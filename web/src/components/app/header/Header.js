b8593c3e14f9eff42db03e506224ed3111ce80b0
Salild1011 <salild1011@gmail.com>
2019-12-07 21:04:50 -0500

    Syntax corrections

--- web/src/components/app/header/Header.js
+++ web/src/components/app/header/Header.js
@@ -4,24 +4,24 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
-const header = (props) => {
+const Header = (props) => {
 
-  return (
-    <Jumbotron className="home-banner">
-      <div className="filter">
-        <div className="banner-content">
-          <h1 className="welcome-title">Welcome to Backrow Shop</h1>
-          <h3 className="welcome-subtitle">Find the best products from the comfort of your home</h3>
+    return (
+        <Jumbotron className="home-banner">
+            <div className="filter">
+                <div className="banner-content">
+                <h1 className="welcome-title">Welcome to Backrow Shop</h1>
+                <h3 className="welcome-subtitle">Find the best products from the comfort of your home</h3>
 
-          <div className="search-container">
-            <input id="search-bar" type="text" onChange={props.inputListener} value={props.search} />
-            <button><FontAwesomeIcon icon="search" /></button>
-          </div>
-          
-        </div>
-      </div>
-    </Jumbotron>
-  );
+                <div className="search-container">
+                    <input id="search-bar" type="text" onChange={props.inputListener} value={props.search} />
+                    <button><FontAwesomeIcon icon="search" /></button>
+                </div>
+                
+                </div>
+            </div>
+        </Jumbotron>
+    );
 }
 
-export default header;
+export default Header;
