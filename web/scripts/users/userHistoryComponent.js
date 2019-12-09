1417c3303bcf167648d17e2e13712e26b6249555
Mengyue <42802258+menyw@users.noreply.github.com>
2019-12-08 20:30:11 -0500

    add log out button to user profile page

--- web/scripts/users/userHistoryComponent.js
+++ web/scripts/users/userHistoryComponent.js
@@ -1,8 +1,8 @@
 module.exports = {
   scope: 'heinz',
   name: 'userHistoryComponent',
-  dependencies: ['Vue'],
-  factory: (Vue) => {
+  dependencies: ['Vue', 'storage','router'],
+  factory: (Vue, storage, router) => {
     'use strict'
 
     var state = { products: [] }
@@ -13,11 +13,19 @@ module.exports = {
           <div class="row">
           <!-- TODO: ADD USER DETAILS -->
           <a href="/userproducts"> View History </a>
+          <button class="btn btn-success" v-on:click="logout">Log out</button>
           </div><!-- /row -->
         </div><!-- /component -->`,
       data: () => {
         return state
       },
+      methods: {
+        logout: function (event) {
+          storage.remove('jwt')
+          storage.remove('user')
+          return router.navigate('/login')
+        }
+      }
     })
 
     const setProducts = (searchResults) => {
