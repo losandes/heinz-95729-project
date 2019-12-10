2a9d217140bd6218c0cede63a3f2e8d7e0a32403
Peter Yefi <peteryef@andrew.cmu.edu>
2019-12-09 20:09:07 -0500

    Removed test module

--- api/tests/books/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  require('../../books/actions/getBook.js'),
-  require('../../books/actions/getBooks.js'),
-  require('../../books/actions/searchBooks.js'),
-  require('../../orders/actions/orderDownload.js')
-]
