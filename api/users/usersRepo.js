ad84e4fe0cd5ec0d55b9a11e61cb89c1288628ae
Peter Yefi <peteryef@andrew.cmu.edu>
2019-12-10 03:25:13 -0500

    Modified usersRepo to require ObjectID from mongodb

--- api/users/usersRepo.js
+++ api/users/usersRepo.js
@@ -1,11 +1,11 @@
 module.exports.name = 'usersRepo'
-module.exports.dependencies = ['db', 'User', '@polyn/blueprint', 'mongodb']
-module.exports.factory = (db, User, _blueprint, mongodb) => {
+module.exports.dependencies = ['db', 'User', '@polyn/blueprint']
+module.exports.factory = (db, User, _blueprint) => {
   'use strict'
 
   const { is } = _blueprint
   const collection = db.collection(User.db.collection)
-  const { ObjectID } = mongodb
+  const ObjectID  = require('mongodb').ObjectID
 
   User.db.indexes.forEach(index => {
     collection.createIndex(index.keys, index.options)
@@ -18,6 +18,10 @@ module.exports.factory = (db, User, _blueprint, mongodb) => {
     return new Promise((resolve, reject) => {
       if (is.not.object(payload)) {
         return reject(new Error('A payload is required to create a User'))
+      }
+
+      if (is.not.string(payload.email)) {
+        return reject(new Error('An email is required to create a User'))
       }
       // en1
       collection.insertOne(payload, (err, res) => {
@@ -34,7 +38,7 @@ module.exports.factory = (db, User, _blueprint, mongodb) => {
     // Get a single user
     */
   const get = (email,password) => {
-  console.log(email, password)
+ 
     return new Promise((resolve, reject) => {
       if (is.not.string(email)) {
         return reject(new Error('An email is required to get a User'))
---
+++ api/users/usersRepo.test.js
@@ -0,0 +1,152 @@
+const dep = require('../test.js')
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+const should = chai.should();
+let User;
+let usersRepoFactory;
+let testUser;
+let testUser2;
+let repo;
+let db;
+
+
+describe('UserRepo Test Suite', function() {
+    before(function(done) {
+      // DB dependency required for userRepo
+      db = dep.connect();
+      db.on('error', (err) => {
+        console.log(err)
+      });
+      db.once('open', function() {
+        done()
+      });
+    
+      // compose the dependencies for usersRepo.js
+     //User dependency required for userRepo
+      User = require('./User.js').factory(dep.blueprint, dep.immutable, 
+        dep.ObjectID, dep.logger)
+      
+      //User Repository (Object under test)
+      usersRepoFactory = require('./usersRepo').factory
+      repo = usersRepoFactory(db, User, dep.blueprint)
+
+      //Sample users to be used for testing
+      testUser = {
+        "name": "test",
+        "email": "test@gmail.com",
+        "password": "123456"
+      }
+
+      testUser2 = {
+        "name": "test",
+        "password": "123456"
+      }
+      
+    });
+
+    //Clean up so that each run of the test begins with the same state of db
+    after(function() {
+      // Clean database here
+      const collection = db.collection(User.db.collection)
+      //delete user created
+      collection.deleteOne({email: testUser.email}, (err, res) => {
+        if (err) {
+          console.log(err)
+        }
+        console.log(`Delete  ${res.result.n} records `)
+      })
+      db.close()
+    });
+  
+    beforeEach(function() {
+      // runs before each test in this block
+    });
+  
+    afterEach(function() {
+      // runs after each test in this block
+    });
+
+    describe('Register User', function() {
+      it('should register a new user', function() {
+     
+      Promise.resolve(repo.create(testUser))
+        .then( user => {
+          user.ops[0].email.should.equal(testUser.email)
+          
+        })
+      });
+    });
+
+    describe('Register User Without Email', function() {
+      it('should return a rejected promise', function() {
+        return expect(repo.create(testUser2))
+          .to.be.rejectedWith('An email is required to create a User');
+        
+      });
+    });
+
+    describe('Register User With Empty Object', function() {
+      it('should reject the promise ', function() {
+        return expect(repo.create(null))
+          .to.be.rejectedWith('A payload is required to create a User');
+        
+      });
+    });
+
+    describe('Get user with email only', function() {
+      it('it should reject the promise', function() {
+        return expect(repo.get(testUser.email))
+          .to.be.rejectedWith('A password is required to get a User');
+        
+      });
+    });
+
+    describe('Get user with password only', function() {
+      it('should reject the promise', function() {
+        return expect(repo.get(null, testUser.password))
+        .to.be.rejectedWith('An email is required to get a User');
+        
+      });
+    });
+
+    describe('Get User With Password and Email', function() {
+      it('should satisfy the promise with a user document', function() {
+        return repo.get(testUser.email, testUser.password)
+          .should.eventually.deep.equal(testUser)
+        
+      });
+    });
+
+    describe('Get User With Unknown Email and Password', function() {
+      it('should satisfy the promise, returning undefined', function() {
+        return repo.get('unknown@gmail.com', '123456')
+          .should.eventually.deep.equal(undefined)
+        
+      });
+    });
+
+    describe('Get User With UserID', function() {
+      it('should satisfy the promise with a user document', function() {
+        return repo.getUserById(testUser._id.toString())
+          .should.eventually.deep.equal(testUser)
+      });
+    });
+
+    describe('Get unknown ID', function() {
+      it('should satisfy the promise, returning undefined', function() {
+        return repo.getUserById('unknowuserId')
+          .should.eventually.deep.equal(undefined)
+      });
+    });
+
+    describe('Get user without an ID', function() {
+      it('should reject the promise', function() {
+        return expect(repo.getUserById())
+        .to.be.rejectedWith('An ID is required to get a User');
+        
+      });
+    });
+  });
+  
