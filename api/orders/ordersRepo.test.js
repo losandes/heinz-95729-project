5d40c0be0428caa8f549b44586aad18ab697718b
Peter Yefi <peteryef@andrew.cmu.edu>
2019-12-11 04:30:45 -0500

    Fixed error related to deleting records after test. Added more tests

--- api/orders/ordersRepo.test.js
+++ api/orders/ordersRepo.test.js
@@ -3,167 +3,156 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const expect = chai.expect;
 chai.use(chaiAsPromised);
-const should = chai.should();
+chai.should();
+const assert = chai.assert;
 let Order;
+let Cart;
 let ordersRepoFactory;
 let testOrder;
-let testOrder2;
 let repo;
 let db;
+let orderID;
 
-
-describe('OrderRepo Test Suite', function () {
-  before(function (done) {
-    // DB dependency required for ordersRepo
-    db = dep.connect();
-    db.on('error', (err) => {
-      console.log(err)
-    });
-    db.once('open', function () {
-      done()
-    });
-
-    
-
-    // Compose the dependencies for ordersRepo.js
-    // Cart dependency required for ordersRepo
-    Cart = require('../shopping-cart/Cart.js').factory(dep.blueprint, dep.immutable,
-      dep.mongodb.ObjectID)
-    // Order dependency required for ordersRepo
-    Order = require('./Order.js').factory(dep.blueprint, dep.immutable, Cart)
-    
-    // Order Repository (Object under test)
-    ordersRepoFactory = require('./ordersRepo').factory
-    repo = ordersRepoFactory(db, Order, dep.blueprint, dep.mongodb)
-
-    // Sample orders to be used for testing
-    testOrder = {
-      "uid": "some_complex_uid3",
-      "total": "80.92",
-      "items": [
-        {
-          "name": "new book 11",
-          "quantity": 2,
-          "price": 20.23,
-          "item_uid": "some_item_uid_63"
-        },
-        {
-          "name": "new book 11",
-          "quantity": 2,
-          "price": 20.23,
-          "item_uid": "some_item_uid"
-        }
-      ]
-    }
-
-
-    // testOrder2 = {
-    //   "name": "test",
-    //   "password": "123456"
-    // }
-
-  });
-
-  //Clean up so that each run of the test begins with the same state of db
-  after(function () {
-    // Clean database here
-    const collection = db.collection(Order.db.collection)
-    
-    // delete order created
-    collection.deleteOne({ uid: "some_complex_uid3" }, (err, res) => {
-      if (err) {
+describe('OrderRepo Test Suite', function() {
+    before(function(done) {
+     
+      db = dep.connect();
+      db.on('error', (err) => {
         console.log(err)
+      });
+      db.once('open', function() {
+        done()
+      });
+    
+      Cart = require('../shopping-cart/Cart.js').factory(dep.blueprint, dep.immutable, dep.mongodb.ObjectID)
+      Order = require('./Order.js').factory(dep.blueprint, dep.immutable, Cart)
+      
+      //User Repository (Object under test)
+      ordersRepoFactory = require('./ordersRepo').factory
+      repo = ordersRepoFactory(db, Order, dep.blueprint, dep.mongodb)
+      
+      testOrder = {
+        "_id": "5dedc8c29d45c73267c999c4",
+        "uid":"564d420206fd376a5b560028",
+        "total":17.54,
+        "items":[
+          {
+            "name":"One Last Thing Before I Go",
+            "quantity":2,
+            "price":8.77,
+            "item_uid":"before_go",
+            "downloads":9
+          }
+        ],
       }
-      console.log(`Delete  ${res.result.n} records `)
-    })
-    db.close()
-  });
+    });
 
-  beforeEach(function () {
-    // runs before each test in this block
-  });
-
-  afterEach(function () {
-    // runs after each test in this block
-  });
-
-  describe('Add an order', function () {
-    it('should add an order to database', function () {
-      Promise.resolve(repo.add(testOrder))
-        .then(order => {
-          console.log(order.ops[0])
-          order.ops[0].uid.should.equal(testOrder.uid)
+    //Clean up so that each run of the test begins with the same state of db
+    after(function() {
+      //Clean database here
+      const collection = db.collection('orders')
+      collection.deleteOne({uid: testOrder.uid}, (err, res) => {
+        if (err) {
+          console.log(err)
+        }
+        console.log(`Deleted  ${res.result.n} order record(s) `)
+      })
+       
+    });
+  
+    describe('Create Order', function() {
+      it('should satisfy the promise returning a document', function() {
+        return Promise.resolve(repo.add(testOrder))
+        .then( order => {
+          orderID = order.ops[0]._id
+          expect(order.ops[0].uid).to.deep.equal(testOrder.uid)
         })
+        
+      });
+    });
+
+    describe('Create order with no payload', function() {
+      it('should return a rejected promise', function() {
+        return expect(repo.add(null))
+          .to.be.rejectedWith('A payload is required to add an order');
+      });
+    });
+
+    describe('Get Order Without User ID', function() {
+      it('it should reject the promise', function() {
+        return expect(repo.get(null))
+          .to.be.rejectedWith('A uid is required to get orders');
+        
+      });
+    });
+
+    describe('Get User Orders with UID', function() {
+      it('should satisfy the promise with orders', function() {
+        return Promise.resolve(repo.get(testOrder.uid))
+        .then( orders => {
+            expect(orders[0].items).to.deep.equal(testOrder.items)
+        })
+      });
+    });
+
+    describe('Get User Order Total Cost', function() {
+      it('should satisfy the promise with correct order total cost', function() {
+        return Promise.resolve(repo.get(testOrder.uid))
+        .then( orders => {
+            expect(orders[0].total).to.deep.equal(testOrder.total)
+        })
+      });
+    });
+
+    describe('Get Order With Unknown UID', function() {
+      it('should satisfy the promise, returning empty array', function() {
+        return repo.get('unknownuid')
+          .should.eventually.deep.equal(Array())
+        
+      });
+    });
+
+    describe('Get Order With Unknown ID', function() {
+      it('should satisfy the promise, returning undefined', function() {
+        return repo.getOne(testOrder._id)
+          .should.eventually.deep.equal(undefined)
+      });
+    });
+
+    describe('Get Order Without ID', function() {
+      it('should reject the promise', function() {
+        console.log(typeof orderID)
+        return expect(repo.getOne())
+        .to.be.rejectedWith('A uid is required to get an order');
+      });
+    });
+
+    describe('Get Order With  UID', function() {
+      it('should satisfy the promise, returning order document', function() {
+        return Promise.resolve(repo.getOne(orderID.toString()))
+        .then( order => {
+          
+            expect(order.items).to.deep.equal(testOrder.items)
+        })
+      });
+    });
+
+    describe('Reduce Item download Quantity', function() {
+      it('should satisfy the promise, returning order document', function() {
+        return Promise.resolve(repo.reduceDownloadQuantity(orderID.toString(), testOrder.items[0].item_uid))
+        .then( result => {
+            expect(result.modifiedCount).to.deep.equal(1)
+        })
+      });
+    });
+
+    describe('Check Amount of Downloads Remaining', function() {
+      it('should satisfy the promise, returning order 8 download for item one', function() {
+        return Promise.resolve(repo.getOne(orderID.toString()))
+        .then( order => {
+            expect(order.items[0].downloads).to.deep.equal(8)
+        })
+      });
     });
   });
-
-  // describe('Register User Without Email', function () {
-  //   it('should return a rejected promise', function () {
-  //     return expect(repo.create(testUser2))
-  //       .to.be.rejectedWith('An email is required to create a User');
-
-  //   });
-  // });
-
-  // describe('Register User With Empty Object', function () {
-  //   it('should reject the promise ', function () {
-  //     return expect(repo.create(null))
-  //       .to.be.rejectedWith('A payload is required to create a User');
-
-  //   });
-  // });
-
-  // describe('Get user with email only', function () {
-  //   it('it should reject the promise', function () {
-  //     return expect(repo.get(testUser.email))
-  //       .to.be.rejectedWith('A password is required to get a User');
-
-  //   });
-  // });
-
-  // describe('Get user with password only', function () {
-  //   it('should reject the promise', function () {
-  //     return expect(repo.get(null, testUser.password))
-  //       .to.be.rejectedWith('An email is required to get a User');
-
-  //   });
-  // });
-
-  // describe('Get User With Password and Email', function () {
-  //   it('should satisfy the promise with a user document', function () {
-  //     return repo.get(testUser.email, testUser.password)
-  //       .should.eventually.deep.equal(testUser)
-
-  //   });
-  // });
-
-  // describe('Get User With Unknown Email and Password', function () {
-  //   it('should satisfy the promise, returning undefined', function () {
-  //     return repo.get('unknown@gmail.com', '123456')
-  //       .should.eventually.deep.equal(undefined)
-
-  //   });
-  // });
-
-  // describe('Get User With UserID', function () {
-  //   it('should satisfy the promise with a user document', function () {
-  //     return repo.getUserById(testUser._id.toString())
-  //       .should.eventually.deep.equal(testUser)
-  //   });
-  // });
-
-  // describe('Get unknown ID', function () {
-  //   it('should satisfy the promise, returning undefined', function () {
-  //     return repo.getUserById('unknowuserId')
-  //       .should.eventually.deep.equal(undefined)
-  //   });
-  // });
-
-  // describe('Get user without an ID', function () {
-  //   it('should reject the promise', function () {
-  //     return expect(repo.getUserById())
-  //       .to.be.rejectedWith('An ID is required to get a User');
-
-  //   });
-  // });
-});
+  
