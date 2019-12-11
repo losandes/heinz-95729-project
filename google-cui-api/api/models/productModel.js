33ef90fe2530500e6feb746946b763ac7e3e4275
nicholeemma <790956043@qq.com>
2019-12-11 11:28:56 -0500

    Changed product schema

--- google-cui-api/api/models/productModel.js
+++ google-cui-api/api/models/productModel.js
@@ -3,20 +3,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-
-var CartSchema = new Schema({
+var ProductSchema = new Schema({
   product_name: {
     type: String,
     required: 'Kindly enter the name of the product',
   },
-  shopping_time: {
-    type: Date,
-    default: Date.now,
-  },
-  unit_number: {
+  product_price: {
     type: Number,
-    default : 1,
+    required: 'Everything needs a price',
   }
 });
 
-module.exports = mongoose.model('Cart', CartSchema);
+module.exports = mongoose.model('Product', ProductSchema);
