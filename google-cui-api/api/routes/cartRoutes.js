b845876ca48f65e79f26713919a128416df7e9a5
nicholeemma <790956043@qq.com>
2019-12-11 11:54:23 -0500

    revised cart routes

--- google-cui-api/api/routes/cartRoutes.js
+++ google-cui-api/api/routes/cartRoutes.js
@@ -1,13 +1,17 @@
 'use strict';
 module.exports = function(app) {
-  
+
   var cart = require('../controllers/cartController');
   // todoList Routes
- 
-  app.route('/cart')
-   .post(cart.add_an_item)
-    .get(cart.list_all_carts)
-    // .put(todoList.update_a_task)
-     .delete(cart.delete_an_item);
 
-}
+  app.route('/cart')
+    .post(cart.add_an_item)
+    .get(cart.list_all_carts)
+    .delete(cart.delete_an_item);
+
+  app.route('/delete/cart')
+    .delete(cart.delete_all_cart);
+
+  app.route('/cart/:user_name')
+    .get(cart.list_user_carts);
+};
