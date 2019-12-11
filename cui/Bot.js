4a9df3723bd791944cfb5996c0f9629f7d622016
Teija812 <ronggui0812@gmail.com>
2019-12-11 15:31:44 -0500

    added dialogflow service account

--- cui/Bot.js
+++ cui/Bot.js
@@ -1,12 +1,18 @@
 module.exports = function Bot (Adventure) {
   const bot = new Adventure()
 
-  bot.question('hello world!')
+  bot.question('how are you today?')
     .then((answer) => {
-      console.log(`You said, ${answer}`)
-      console.log()
-      console.log('Checkout `https://github.com/losandes/terminal-adventure` for more information on how to use Terminal Adventure')
-      console.log()
-      bot.complete()
+        if (answer==="don't repeat"){
+            console.log('i can only repeat')
+        }
+        else{
+            console.log(`You said, ${answer}`)
+        }
+        console.log()
+        console.log('Checkout `https://github.com/losandes/terminal-adventure` for more information on how to use Terminal Adventure')
+        console.log()
+        bot.complete()
     })
+
 }
