2995296e65389f4f5a1e871ebb7d23c857d14704
alejandroxag <alejandro.alvarez.1804@gmail.com>
2021-11-26 23:04:47 -0500

    changes to topic modeling function

--- api/ml-jokes/mljokes/topics.py
+++ api/ml-jokes/mljokes/topics.py
@@ -3,7 +3,7 @@ from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Get topic distribution per joke
-def get_lda_topics(text, n_topics=5, random_state=0):
+def get_lda_topics(text, ids, n_topics=5, random_state=0):
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(text)
     vectorizer.get_feature_names_out()
@@ -11,5 +11,6 @@ def get_lda_topics(text, n_topics=5, random_state=0):
     # Find latent topics
     lda = LatentDirichletAllocation(n_components=n_topics,random_state=random_state)
     X_topics = lda.fit_transform(X)
+    X_topics.insert(0, 'id', ids)
 
     return X_topics
