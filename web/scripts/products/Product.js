0e27fd5f2d7a6f0ae63412433836524fa6081219
ipalchou <ipalchou@andrew.cmu.edu>
2021-12-02 18:20:31 -0500

    add to cart

--- web/scripts/products/Product.js
+++ web/scripts/products/Product.js
@@ -1,55 +1,49 @@
 module.exports = {
-  scope: 'heinz',
-  name: 'Product',
-  dependencies: ['router'],
-  factory: (router) => {
-    'use strict'
+    scope: 'heinz',
+    name: 'Product',
+    dependencies: ['router'],
+    factory: (router) => {
+        'use strict'
 
-    return function Product(product) {
-      product = Object.assign({}, product)
+        return function Product(product) {
+            product = Object.assign({}, product)
 
-      const self = {
-        type: product.type || 'product',
-        id: product.id,
-        uid: product.uid,
-        title: product.title,
-        description: product.description,
-        metadata: product.metadata,
-        price: product.price,
-        images: [],
-        thumbnailHref: product.thumbnailHref || '/images/products/default.png',
-        thumbnailAlt: `thumbnail for ${product.title}`,
-        showThumbnail: product.thumbnailHref != null,
-        detailsLink: `/${product.type}/${product.uid}`,
-      }
+            const self = {
+                type: product.type || 'product',
+                id: product.id,
+                uid: product.uid,
+                title: product.title,
+                description: product.description,
+                metadata: product.metadata,
+                price: product.price,
+                images: [],
+                thumbnailHref: product.thumbnailHref || '/images/products/default.png',
+                thumbnailAlt: `thumbnail for ${product.title}`,
+                showThumbnail: product.thumbnailHref != null,
+                detailsLink: `/${product.type}/${product.uid}`,
+            }
 
-      self.viewDetails = (event) => {
-        if (!self.uid) {
-          // this must be the default VM
-          return
+            self.viewDetails = (event) => {
+                if (!self.uid) {
+                    // this must be the default VM
+                    return
+                }
+
+                switch (self.type) {
+                    case 'book':
+                        router.navigate(`/books/${self.uid}`)
+                        break
+                    default:
+                        router.navigate(`/products/${self.uid}`)
+                        break
+                }
+            }
+
+            self.addToCart = (event) => {
+                router.navigate(`/cart-upsert/${self.id}`)
+            }
+
+            return self
         }
-
-        switch (self.type) {
-          case 'book':
-            router.navigate(`/books/${self.uid}`)
-            break
-          default:
-            router.navigate(`/products/${self.uid}`)
-            break
-        }
-      }
-
-      self.addToCart = (event) => {
-        let currentBook = {title: self.title,
-          price: self.price,
-          id: self.id,
-          inCart: 0
-        };
-
-        setItems(currentBook);
-      }
-
-      return self
-    }
-  },
+    },
 }
--- web/scripts/products/productsRepo.js
+++ web/scripts/products/productsRepo.js
@@ -1,20 +1,23 @@
 module.exports = {
-  scope: 'heinz',
-  name: 'productsRepo',
-  dependencies: ['Repo'],
-  factory: (Repo) => {
-    'use strict'
+    scope: 'heinz',
+    name: 'productsRepo',
+    dependencies: ['Repo'],
+    factory: (Repo) => {
+        'use strict'
 
-    const repo = new Repo()
+        const repo = new Repo()
 
-    const get = (uid, callback) => {
-      repo.get({ path: `/products/${uid}` }, callback)
-    }
+        const get = (uid, callback) => {
+            repo.get({ path: `/products/${uid}` }, callback)
+        }
 
-    const search = (query, callback) => {
-      repo.get({ path: `/products?q=${query}` }, callback)
-    }
+        const search = (query, callback) => {
+            repo.get({ path: `/products?q=${query}` }, callback)
+        }
+        const addToCart = (pid, callback) => {
+            repo.get({ path: `/cart-upsert/${pid}` }, callback)
+        }
 
-    return { get, search }
-  },
+        return { get, search, addToCart }
+    },
 }
