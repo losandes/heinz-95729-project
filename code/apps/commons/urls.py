1401c6933b47feb04065ac5eb96fc2090cf8f5ef
johnny.liu <johnny.liu@connext.com.cn>
2022-12-12 02:15:02 -0500

    feat: add autocomplete search view and related page
    Solved conflic with dev

--- code/apps/commons/urls.py
+++ code/apps/commons/urls.py
@@ -6,10 +6,10 @@ from apps.commons.views_products import ProductPageViews
 from apps.commons.views_orders import OrderPageViews
 from apps.commons.views_carts import CartPageViews
 from apps.commons.views_payment import PaymentPageViews
-from apps.commons.views_oauth import AuthViews
 from apps.commons.views_search import SearchPageViews
 from apps.commons.views_bot import BotPageViews
 from apps.commons.views_oauth import AuthViews
+from apps.commons.views_search import SearchPageViews
 
 urlpatterns = [
     path('admins/<str:page>', CommonPageViews.admin_page, name='admin_page'),
@@ -71,14 +71,6 @@ urlpatterns = [
 
     # Bot Domain
     path('store/message', BotPageViews.get_message_from_bot, name='get_message_from_bot'),
-
-    # OAuth Domain
-    path('store/google/login', AuthViews.google_login, name='store_google_login'),
-    path('store/google/login/callback', AuthViews.google_login_callback, name='store_google_login_callback'),
-    path('store/reddit/login', AuthViews.reddit_login, name='store_reddit_login'),
-    path('store/reddit/login/callback', AuthViews.reddit_login_callback, name='store_reddit_login_callback'),
-    path('store/github/login', AuthViews.github_login, name='store_github_login'),
-    path('store/github/login/callback', AuthViews.github_login_callback, name='store_github_login_callback'),
 
     # Manual Operation
     path('operation/spider_execute', ManualOperationPageView.spider_execute),
