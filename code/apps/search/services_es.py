3ba39672c998173140e0bba93781254d1515a310
johnny.liu <johnny.liu@connext.com.cn>
2022-12-12 02:11:36 -0500

    feat: add service layer for autocomplete search

--- code/apps/search/services_es.py
+++ code/apps/search/services_es.py
@@ -1,5 +1,5 @@
 
-from config.settings.config_es import ES_URL
+from config.settings.config_es import ES_URL, ES_INDEX_PRODUCTS
 from elasticsearch.client import Elasticsearch
 
 import logging
@@ -8,4 +8,20 @@ logger = logging.getLogger(__name__)
 es = Elasticsearch(hosts=[ES_URL])
 
 class ESSearchProcessor:
-    pass
+    def autocomplete_search(query):
+        try:
+            res = es.search(index=ES_INDEX_PRODUCTS, body={
+                "query": {
+                    "multi_match": {
+                        "query": query,
+                        "fields": ["name", "chinese_name"]
+                    }
+                }
+            })
+            for hit in res["hits"]["hits"]:
+                hit["source"] = hit["_source"]
+                del hit["_source"]
+            return res
+        except Exception as e:
+            logger.error("Error while searching for autocomplete: " + str(e))
+            return None
