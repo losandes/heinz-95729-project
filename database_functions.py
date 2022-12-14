3f7bf0b8775394b9f5aac8c11674796c633b699e
ewclark47 <93678940+ewclark47@users.noreply.github.com>
2022-12-14 10:53:50 -0500

    docs: remove some print statements

--- database_functions.py
+++ database_functions.py
@@ -25,50 +25,50 @@ cursor = mydb.cursor()
 # If so, return True. If not, add the user to the database and then 
 # return True
 def checkUser(username):
-    #print(username)
-    #print(type(username))
+    ##print(username)
+    ##print(type(username))
     queryString = "SELECT * FROM users WHERE UserName=\'"+str(username)+"\'"
     cursor.execute(queryString)
     validUser = cursor.fetchall()
-    print(validUser)
+    #print(validUser)
     if len(validUser)>0:
-        print("This is happening in checkUser first part")
+        #print("This is happening in checkUser first part")
         return True
     else:
-        print("It went on to the checkUser second part")
+        #print("It went on to the checkUser second part")
         queryString = "INSERT INTO users(UserName) VALUES (\'"+str(username)+"\')"
         cursor.execute(queryString)
         mydb.commit()
         queryString = "SELECT * FROM users WHERE UserName=\'"+str(username)+"\'"
         cursor.execute(queryString)
         addedUser = cursor.fetchall()
-        print("Second part of checkUser ran")
-        print(addedUser[0][0])
-        print("added user: " + str(addedUser))
+        #print("Second part of checkUser ran")
+        #print(addedUser[0][0])
+        #print("added user: " + str(addedUser))
         return True
 
 # Check to see if the user that mentioned ScottyBot has a schedule
 # If so, return True. If not, add a schedule for the user to the database and then 
 # return True
 def checkUserSchedule(userID):
-    print(userID)
+    #print(userID)
     queryString = "SELECT * FROM schedules WHERE UserID="+str(userID)
     cursor.execute(queryString)
     scheduleDoesExist = cursor.fetchall()
     if len(scheduleDoesExist)>0:
-        print("First part of Schedule Check, it exists")
+        #print("First part of Schedule Check, it exists")
         return True
     else:
         # add in a schedule for this user with default values
-        print("Went on to checkUserSchedule second part")
+        #print("Went on to checkUserSchedule second part")
         queryString = "INSERT INTO schedules(UserID) VALUES ("+str(userID)+")"
         cursor.execute(queryString)
         mydb.commit()
-        print("Added row to schedules with this users userID")
+        #print("Added row to schedules with this users userID")
         return True
 
 def addCourse(username, courseNumber):
-    print("Start of addCourse database function")
+    #print("Start of addCourse database function")
     try:
         queryString = "SELECT * FROM course WHERE CourseNumber=" + str(courseNumber)
         cursor.execute(queryString)
@@ -78,32 +78,32 @@ def addCourse(username, courseNumber):
         for r in courseInfo:
             new_str += str(r)+", "
         courseInfo = new_str
-        print("Got the course info")
+        #print("Got the course info")
         if checkUser(username):
-            print("Validated or added the user")
+            #print("Validated or added the user")
             queryString = "SELECT UserID FROM users WHERE UserName=\'"+str(username)+"\'"
             cursor.execute(queryString)
             userID = cursor.fetchone()
             userID = userID[0]
-            print("Got the users ID")
+            #print("Got the users ID")
             # get how many courses this user has (IF THEY HAVE THEM) and then add in the request course as 'course#' based on that number
             if checkUserSchedule(userID):
-                print("Validated or added schedule for this user")
+                #print("Validated or added schedule for this user")
                 queryString = "SELECT CourseCount FROM schedules WHERE UserID="+str(userID)
                 cursor.execute(queryString)
                 courseCount = cursor.fetchone()
-                print("Got the Course Count " + str(courseCount[0]))
+                #print("Got the Course Count " + str(courseCount[0]))
                 courseCount = courseCount[0]+1
                 cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='schedules'")
                 scheduleColumns = cursor.fetchall()
                 columnList =[]
                 for column in scheduleColumns:
                     columnList.append(column[0])
-                print("All existing columns retrieved")
-                print(columnList)
+                #print("All existing columns retrieved")
+                #print(columnList)
                 updateCol = "Course " + str(courseCount)
-                print(updateCol)
-                print(updateCol in columnList)
+                #print(updateCol)
+                #print(updateCol in columnList)
                 if updateCol in columnList:
                     queryString = "UPDATE schedules SET `Course "+str(courseCount)+"`=\'"+str(courseInfo)+"\' WHERE UserID="+str(userID)
                     cursor.execute(queryString)
@@ -117,13 +117,13 @@ def addCourse(username, courseNumber):
                     queryString = "UPDATE schedules SET CourseCount="+str(courseCount)+" WHERE UserID="+str(userID)
                     cursor.execute(queryString)
                     mydb.commit() # manual commit added because database was not receiving autocommit from update query
-                    print("This users course count after adding a course: " + str(courseCount))
+                    #print("This users course count after adding a course: " + str(courseCount))
                     queryString = "UPDATE schedules SET `Course "+str(courseCount)+"`=\'"+str(courseInfo)+"\' WHERE UserID="+str(userID)
                     cursor.execute(queryString)
                     mydb.commit() # manual commit added because database was not receiving autocommit from update query
-                    print("added: " + str(courseInfo) + "to the users schedule")
-        else:
-            print("Issue with the username")
+                    #print("added: " + str(courseInfo) + "to the users schedule")
+            else:
+                print("Issue with the username")
 
     except mysql.connector.Error as e:
         print(e)
@@ -148,7 +148,7 @@ def dropCourse(username, courseNumber):
         columnList =[]
         for column in scheduleColumns:
             columnList.append(column[0])
-        print("All existing columns retrieved")
+        #print("All existing columns retrieved")
         cursor.execute("SELECT * FROM schedules WHERE UserID="+str(userID))
         scheduleRow=cursor.fetchone()
         scheduleValues = []
@@ -165,7 +165,7 @@ def dropCourse(username, courseNumber):
             cursor.execute("SELECT `Course "+str(i)+"` FROM schedules WHERE UserID="+str(userID))
             info = cursor.fetchone()
             courses.append(info[0])
-        print(courses)
+        #print(courses)
         courses.remove(' ')
         for i in range(len(courses)):
             cursor.execute("UPDATE schedules SET `Course "+str(i+1)+"`=\'"+str(courses[i])+"\' WHERE UserID="+str(userID))
@@ -187,14 +187,14 @@ def viewSchedule(username):
             userID = cursor.execute(queryString)
             userID = cursor.fetchone()
             userID=userID[0]
-            print("User's ID is: " + str(userID))
+            #print("User's ID is: " + str(userID))
             if checkUserSchedule(userID):
                 queryString = "SELECT * FROM schedules WHERE UserID="+str(userID)
                 cursor.execute(queryString)
                 row = cursor.fetchone()
-                #print(row[3:])
+                ##print(row[3:])
                 scheduleString=""
-                print("start loop through schedule")
+                #print("start loop through schedule")
                 if len(row[3:])>0:
                     for r in row[3:]:
                         if r == "" or r == " ":
@@ -203,11 +203,11 @@ def viewSchedule(username):
                             scheduleString += "\n" + str(r)
                     if scheduleString=="":
                         scheduleString="You haven't scheduled anything yet!"
-                        print(scheduleString)
+                        #print(scheduleString)
                 else:
                     scheduleString = "You haven't scheduled anything yet!"
-                    print(scheduleString)
-        print(scheduleString)
+                    #print(scheduleString)
+        #print(scheduleString)
         return scheduleString
         
     except mysql.connector.Error as e:
@@ -225,11 +225,11 @@ def findCourse(description):
             for c in course[1:]:
                 useableString += str(c) + " "
                 courseString += str(c).lower() + " "
-            #print(courseString)
+            ##print(courseString)
             if str(description).lower() in courseString:
                 possibleCourse += useableString + "\n"
-        print("Possible course(s): ")
-        print(possibleCourse)
+        #print("Possible course(s): ")
+        #print(possibleCourse)
         return possibleCourse
     
     except mysql.connector.Error as e:
@@ -241,17 +241,17 @@ def method_tests():
     try:
         cursor = mydb.cursor()
         # test using partial course number -> shows all
-        """ print("Finding a course using partial course number: ")
+        """ #print("Finding a course using partial course number: ")
         findCourse(95) """
         # test using partial course name "Machine Learning" -> shows ecomm and MLPS
-        print("Finding a course using a topic: ")
-        findCourse("Design")
+        #print("Finding a course using a topic: ")
+        #findCourse("Design")
         # test find all courses -> works
-        """ print("Finding all courses: ")
+        """ #print("Finding all courses: ")
         findCourse(" ") """
 
         # test viewing an empty schedule -> works
-        #print(viewSchedule("nonexistuser"))
+        ##print(viewSchedule("nonexistuser"))
         # test adding a course when others have more courses -> works
         #addCourse("testuserrando", 95729)
         # test dropping a course -> works
@@ -262,17 +262,17 @@ def method_tests():
         addCourse("randotestuser",95729)
         addCourse("randotestuser",95828)
         dropCourse("randotestuser",95702)
-        print(viewSchedule("randotestuser"))
+        #print(viewSchedule("randotestuser"))
         addCourse("randotest",95702)
         addCourse("randotest",95729)
         addCourse("randotest",95828)
         dropCourse("randotest",95729)
-        print(viewSchedule("randotest"))
+        #print(viewSchedule("randotest"))
         addCourse("randouser",95702)
         addCourse("randouser",95729)
         addCourse("randouser",95828)
         dropCourse("randouser",95828)
-        print(viewSchedule("randouser")) """
+        #print(viewSchedule("randouser")) """
         
     except mysql.connector.Error as e:
         print(e)
