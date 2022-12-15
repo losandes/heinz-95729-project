f50803354ae5074cb46e53d6ec175bed16370306
“kimgogowow” <kimwu9419@gmail.com>
2022-12-14 22:17:03 -0500

    fit: cart domain file update

--- api/src/lib/carts/migrations/order.js
+++ api/src/lib/carts/migrations/order.js
@@ -1,4 +1,5 @@
 exports.up = async function (knex) {
+  const time_added_ms = Date.now()
   const sql = `CREATE TABLE orderhistory (
       id UUID PRIMARY KEY NOT NULL,
       productId TEXT NOT NULL,
@@ -7,7 +8,7 @@ exports.up = async function (knex) {
 
   );
 
-  COMMENT ON COLUMN orderhistory.id IS 'The record id';
+  COMMENT ON COLUMN orderhistory.id IS 'The record id';s
   COMMENT ON COLUMN orderhistory.productId IS 'The product id';
   COMMENT ON COLUMN orderhistory.userId IS 'The user ID';
   COMMENT ON COLUMN orderhistory.timestamp_ms IS 'The time of purchase, in milliseconds';`
