d2bae688bcb13cf2b1c843cdc113eb1b7ad0ce6b
HSunSK <howardsunacc@gmail.com>
2022-12-14 21:08:13 -0500

    Create Carts Domain
    Create carts domain and start modifying files. Initial modification for cart.js

---
+++ api/src/lib/carts/src/Cart.js
@@ -0,0 +1,30 @@
+/**
+ * @param {@polyn/blueprint} blueprint
+ * @param {@polyn/immutable} immutable
+ * @param {uuid/v4} uuid
+ */
+function CartFactory (deps) {
+  'use strict'
+
+  const { registerBlueprint, optional } = deps.blueprint
+  const { immutable } = deps.immutable
+  const { uuid } = deps
+
+  const REGEX = {
+    UUID: /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+  }
+
+  const CartBlueprint = {
+    id: optional(REGEX.UUID).withDefault(uuid),
+    productId: 'string',
+    userId: 'string',
+  }
+
+  registerBlueprint('Cart', CartBlueprint)
+  const Cart = immutable('Order', CartBlueprint)
+  Cart.blueprint = CartBlueprint
+
+  return { Cart }
+}
+
+module.exports = CartFactory
