85a5a662d8f2fc767453478478821376a49481bb
Ahmad Saadat <ahmads@iastate.edu>
2022-12-15 21:13:36 -0500

    welcome and fallback implemented & tested

--- src/index.js
+++ src/index.js
@@ -1,92 +1,47 @@
 // // const dialogflow = require('dialogflow');
 
-const { dialogflow, Image } = require('actions-on-google');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const uri =
-    'mongodb+srv://ecomm:0Ax6t45R3tdgU8oR@cluster0.z4xh1w1.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
-
-async function insert_cart_item(user_id, items) {
-    var rec = {
-        user_id: user_id,
-        items,
-        placed: false,
-    };
-
-    try {
-        await client.connect();
-        const query = { user_id: user_id, placed: false };
-        newdoc = await client.db('main').collection('order').findOne(query);
-
-        if (newdoc == null) {
-            await client.db('main').collection('order').insertOne(rec).catch();
-        } else {
-            const update_query = { user_id: user_id };
-
-            // add the current coffee into the document
-            await client
-                .db('main')
-                .collection('order')
-                .updateOne(update_query, {
-                    $push: { items: { $each: items } },
-                });
-
-            // aggregate duplicated coffees
-            const agg = [
-                { $unwind: '$items' },
-                {
-                    $group: {
-                        _id: '$items.coffee',
-                        number: { $sum: '$items.number' },
-                    },
-                },
-                {
-                    $group: {
-                        _id: 0,
-                        items: { $push: { coffee: '$_id', number: '$number' } },
-                    },
-                },
-                { $project: { _id: 0, items: 1 } },
-            ];
-
-            const result = await client
-                .db('main')
-                .collection('order')
-                .aggregate(agg);
-
-            for await (const doc of result) {
-                await client
-                    .db('main')
-                    .collection('order')
-                    .updateOne({ user_id: user_id }, { $set: doc });
-            }
-        }
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-
+const {
+    dialogflow,
+    Image,
+    BasicCard,
+    Suggestions,
+} = require('actions-on-google');
 const app = dialogflow({ debug: true });
+
 console.log('in index.js');
 
 app.intent('Default Welcome Intent', (conv) => {
-    console.log(JSON.stringify(conv));
-
-    console.log('in default welcome intent handler');
-    conv.ask('Hello from the a team!');
+    conv.ask(
+        'Welcome to Starbux! You can say things like: Hear menu or place an order'
+    );
+    conv.ask(
+        new BasicCard({
+            text: `Welcome to Starbux! You can say things like: Hear menu or place an order`,
+            title: 'Welcome to Starbux!',
+            image: new Image({
+                url: 'https://ecomm-starbux-artifact-bucket.s3.amazonaws.com/starbux_welcome_picture.jpg',
+                alt: 'Welcome_To_Starbux_Image',
+            }),
+        })
+    );
+    conv.ask(new Suggestions(['Hear Menu', 'Place an order']));
 });
 
 app.intent('Default Fallback Intent', (conv) => {
-    conv.ask(`Sorry I didn't get that`);
     conv.ask(
-        new Image({
-            url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-            alt: 'A cat',
+        `Sorry I didn't get that, you can say things like Hear the menu or place an order`
+    );
+    conv.ask(
+        new BasicCard({
+            text: `Sorry I didn't get that, you can say things like Hear the menu or place an order`,
+            title: `Sorry I didn't get that 😔`,
+            image: new Image({
+                url: 'https://ecomm-starbux-artifact-bucket.s3.amazonaws.com/starbux_sad_but_rad.png',
+                alt: 'Starbux_sad_but_rad',
+            }),
         })
     );
+    conv.ask(new Suggestions(['Hear Menu', 'Place an order']));
 });
 
 app.intent('order.additem', (conv) => {
@@ -95,12 +50,12 @@ app.intent('order.additem', (conv) => {
     // user id is assumed to be always one
     // this isn't correct in production, but within the scope of project, user
     // authentication / authorization will be too much work
-    user_id = 1;
+    // user_id = 1;
 
-    params = conv.body.queryResult.parameters;
-    items = params['number-coffee'];
+    // params = conv.body.queryResult.parameters;
+    // items = params['number-coffee'];
 
-    insert_cart_item(user_id, items);
+    // insert_cart_item(user_id, items);
 
     conv.ask('Coffee added to order.');
 });
