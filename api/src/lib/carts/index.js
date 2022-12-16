32946b5ff4c321bb3b1097b1093850933fa79b5d
jahnvigupta8 <jahnvigupta8@gmail.com>
2022-12-15 19:18:19 -0500

    changes in Index.js -> Cart

--- api/src/lib/carts/index.js
+++ api/src/lib/carts/index.js
@@ -1,26 +1,31 @@
 const blueprint = require('@polyn/blueprint')
 const immutable = require('@polyn/immutable')
+const path = require('path')
 const uuid = require('uuid').v4
 const { Cart } = require('./src/Cart.js')({ blueprint, immutable, uuid })
-const { CartPgRepo } = require('./src/CartPgRepo.js')({ Cart })
-const { AddToCart } = require('./src/add-to-cart.js')()
-const { RemoveProduct } = require('./src/remove-product.js')()
+const { CartPgRepo } = require('./src/CartPgRepo.js')({ blueprint, Cart })
+const AddToCartFactory = require('./src/add-to-cart.js')
+const RemoveProductFactory = require('./src/remove-product.js')
+const MigrateCartFactory = require('./migrate.js')
 
 /**
  * @param {knex} knex - A configured/initialized instance of knex
  */
 function CartsFactory (input) {
   const cartRepo = new CartPgRepo({ knex: input.knex })
-  const { addToCart } = new AddToCart({ cartRepo })
-  const { removeProduct } = new RemoveProduct({ cartRepo })
+  const { addToCart } = new AddToCartFactory({ cartRepo })
+  const { removeProduct } = new RemoveProductFactory({ cartRepo })
+  const migrate = MigrateCartFactory({ path, knex: input.knex })
 
   return {
     Cart,
     cartRepo,
     addToCart,
     removeProduct,
+    migrate,
   }
 }
 
 module.exports = CartsFactory
+
 
