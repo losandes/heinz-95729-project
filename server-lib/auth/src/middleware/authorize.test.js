5634df698ddee139195e76ac8f8a49e0031c1609
losandes <andes.collab@gmail.com>
2023-03-11 18:59:33 -0500

    feat: adds tests for authorize and deauthorize

---
+++ server-lib/auth/src/middleware/authorize.test.js
@@ -0,0 +1,56 @@
+import test from 'test'
+import expect from 'unexpected'
+import { authorize } from '@heinz-95729/auth'
+
+const mockCtx = () => {
+  /** @type {Object.<string, string>} */
+  const headers = {}
+  const set = (
+    /** @type {string} */ key,
+    /** @type {string} */ value,
+  ) => { headers[key] = value }
+  const response = { headers, set, body: null, status: 0 }
+
+  return { response }
+}
+
+const given = '@heinz-95729/auth authorize:'
+
+const when = {
+  calledWithValidRedirectURL: {
+    c: `${given} when auth is called with a valid redirectURL`,
+    s: async () => {
+      const redirectURL = 'http://localhost:3000/authorize'
+      const route = authorize(redirectURL)
+      const ctx = mockCtx()
+      // @ts-ignore
+      await route(ctx)
+
+      return { ctx, redirectURL }
+    },
+  },
+}
+
+test(when.calledWithValidRedirectURL.c +
+  'it should add an HTML page to the response body',
+async () => {
+  const { ctx, redirectURL } = await when.calledWithValidRedirectURL.s()
+
+  expect(ctx.response.body, 'to contain', '<html>')
+  expect(ctx.response.body, 'to contain', redirectURL)
+})
+
+test(when.calledWithValidRedirectURL.c +
+  'it should set the Content-Type header',
+async () => {
+  const { ctx } = await when.calledWithValidRedirectURL.s()
+
+  expect(ctx.response.headers['Content-Type'], 'to equal', 'text/html')
+})
+
+test(when.calledWithValidRedirectURL.c +
+  'it should set the status to 200',
+async () => {
+  const { ctx } = await when.calledWithValidRedirectURL.s()
+  expect(ctx.response.status, 'to equal', 200)
+})
