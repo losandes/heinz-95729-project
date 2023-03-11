a712dd8dd4249f5891b5d8736aca5ba2772504db
losandes <andes.collab@gmail.com>
2023-03-11 18:05:42 -0500

    feat: replaces blueprint with zod for envvars
    This replaces @polyn/blueprint for validating the ENVVARs on app
    startup. The new validation library is zod. I also updated the
    documentation.

--- server/src/env.d.js
+++ server/src/env.d.js
@@ -3,25 +3,25 @@
  * The environment variables used to configure the app and
  * requests to the app.
  *
- * - `NODE_ENV`: the environment the app is running in: /^(local|test|development|production)$/
- * - `NODE_ENV_OPTIONS`: constants for conditionally addressing NODE_ENV: LOCAL, TEST, DEV, PROD
- * - `ENFORCE_HTTPS`: whether or not to enforce HTTPS or allow HTTP requests
- * - `ALLOW_DEV_CONFIGURATIONS`: turn dev features on and off, such as GraphiQL, and the Content-Security-Policies that go with that
- * - `APP_VERSION`: the version as stated in the package.json file
- * - `PORT`: the port this app can be reached at: default='3001'
- * - `ROUTER_PREFIX`: the path this api can be reached at when it's running in proxy mode: default=''
- * - `APP_IS_IN_PROXY`: whether or not the app is running in proxy mode: conditionally set to true when the value of ROUTER_PREFIX is not an empty string
- * - `WEB_APP_ORIGIN`: the HTTP origin of the web app that uses this api: default='http://localhost:3001'
- * - `CORS_ORIGIN`: the HTTP origin to allow with CORS: default=`${WEB_APP_ORIGIN}`
- * - `JWT_COOKIE_NAME`: the name of the cookie that will be sent to the browser: default='h95729'
- * - `JWT_SECRET`: a 256+ bit (32+ char) cryptographic secret to sign JWT tokens with
- * - `JWT_EXPIRES_IN`: JWT Expiry in the format that jsonwebtoken wants it: make sure it matches  JWT_EXPIRES_IN_MS!: default='30d'
- * - `JWT_EXPIRES_IN_MS`: Cookie Expiry in the millisecond format that koa's ctx.cookies.set wants it: make sure it matches JWT_EXPIRES_IN!: default=(86400000 * 30) (30 days)
- * - `LOG_WRITER`: the typeof writer to emit logs to: default='DevConsoleWriter'] - /^(ArrayWriter|ConsoleWriter|DevConsoleWriter|StdoutWriter)$/
- * - `LOG_FORMATTER`: the typeof formatter to format the logs with: default='BlockFormatter'] - /^(BlockFormatter|BunyanFormatter|JsonFormatter|PassThroughFormatter|StringFormatter|SquashFormatter)$/
- * - `LOG_LISTENERS`: the log events to listen for: default=['local', 'todo', 'trace', 'debug', 'info', 'warn', 'error', 'fatal']]
- * - `PATH_TO_KEYV_DB`: the path to the database file (e.g. sqlite://path/to/data.db)
- * - `PATH_TO_GRAPHQL_SCHEMA`: the path to the GraphQL schema (e.g. schema/schema.graphql)
+ * - NODE_ENV: 'the environment the app is running in: /^(local|test|development|production)$/',
+ * - NODE_ENV_OPTIONS: 'constants for conditionally addressing NODE_ENV: LOCAL, TEST, DEV, PROD',
+ * - APP_VERSION: 'the version as stated in the package.json file',
+ * - ALLOW_DEV_CONFIGURATIONS: 'Used to turn dev features on and off, such as GraphiQL, and the Content-Security-Policies that go with that',
+ * - ENFORCE_HTTPS: 'whether or not to enforce HTTPS or allow HTTP requests',
+ * - PORT: 'the port this app can be reached at: default=3001',
+ * - ROUTER_PREFIX: 'optional path this api can be reached at when it\'s running in proxy mode',
+ * - APP_IS_IN_PROXY: 'whether or not the app is running in proxy mode: conditionally set to true when the value of ROUTER_PREFIX is not an empty string',
+ * - WEB_APP_ORIGIN: 'the HTTP origin of the web app that uses this api: default=http://localhost:3001',
+ * - CORS_ORIGIN: 'the HTTP origin to allow with CORS: default={WEB_APP_ORIGIN}',
+ * - JWT_COOKIE_NAME: 'the name of the cookie that will be sent to the browser: default=h95729',
+ * - JWT_SECRET: 'a 256+ bit (32+ char) cryptographic secret to sign JWT tokens with',
+ * - JWT_EXPIRES_IN: 'JWT Expiry in the format that jsonwebtoken wants it: make sure it matches  JWT_EXPIRES_IN_MS!',
+ * - JWT_EXPIRES_IN_MS: 'Cookie Expiry in the millisecond format that koa\'s ctx.cookies.set wants it: make sure it matches JWT_EXPIRES_IN! ',
+ * - LOG_WRITER: 'the typeof writer to emit logs to',
+ * - LOG_FORMATTER: 'the typeof formatter to format the logs with',
+ * - LOG_LISTENERS: 'The events to pipe to the default logger',
+ * - PATH_TO_KEYV_DB: 'the path to the database file (e.g. sqlite://path/to/data.db)',
+ * - PATH_TO_GRAPHQL_SCHEMA: 'the path to the GraphQL schema (e.g. schema/schema.graphql)',
  * @typedef {Object} IENVVARS
  * @property {string} NODE_ENV
  * @property {INodeEnvOptions} NODE_ENV_OPTIONS
@@ -63,7 +63,7 @@
 /**
  * The static properties defined on ENVVARS
  * @typedef {Object} _ENVVARSStaticProps
- * @property {any} blueprint
+ * @property {any} schema
  */
 
 /**
--- server/src/env.js
+++ server/src/env.js
@@ -1,162 +1,109 @@
-import {
-  is,
-  optional,
-  registerValidator,
-  registerBlueprint,
-  required,
-} from '@polyn/blueprint'
-import { immutable } from '@polyn/immutable'
+import { immutable } from '@heinz-95729/immutable'
+import { z } from 'zod'
 
-registerValidator('256bitString', ({ key, value }) => {
-  if (
-    typeof value === 'string' &&
-    Buffer.byteLength(value.trim(), 'utf8') >= 32 // 32 utf8 chars = 256 bits
-  ) {
-    return { value }
-  }
+const DESC = {
+  NODE_ENV: 'the environment the app is running in: /^(local|test|development|production)$/',
+  NODE_ENV_OPTIONS: 'constants for conditionally addressing NODE_ENV: LOCAL, TEST, DEV, PROD',
+  APP_VERSION: 'the version as stated in the package.json file',
+  ALLOW_DEV_CONFIGURATIONS: 'Used to turn dev features on and off, such as GraphiQL, and the Content-Security-Policies that go with that',
+  ENFORCE_HTTPS: 'whether or not to enforce HTTPS or allow HTTP requests',
+  PORT: 'the port this app can be reached at: default=3001',
+  ROUTER_PREFIX: 'optional path this api can be reached at when it\'s running in proxy mode',
+  APP_IS_IN_PROXY: 'whether or not the app is running in proxy mode: conditionally set to true when the value of ROUTER_PREFIX is not an empty string',
+  WEB_APP_ORIGIN: 'the HTTP origin of the web app that uses this api: default=http://localhost:3001',
+  CORS_ORIGIN: 'the HTTP origin to allow with CORS: default={WEB_APP_ORIGIN}',
+  JWT_COOKIE_NAME: 'the name of the cookie that will be sent to the browser: default=h95729',
+  JWT_SECRET: 'a 256+ bit (32+ char) cryptographic secret to sign JWT tokens with',
+  JWT_EXPIRES_IN: 'JWT Expiry in the format that jsonwebtoken wants it: make sure it matches  JWT_EXPIRES_IN_MS!',
+  JWT_EXPIRES_IN_MS: 'Cookie Expiry in the millisecond format that koa\'s ctx.cookies.set wants it: make sure it matches JWT_EXPIRES_IN! ',
+  LOG_WRITER: 'the typeof writer to emit logs to',
+  LOG_FORMATTER: 'the typeof formatter to format the logs with',
+  LOG_LISTENERS: 'The events to pipe to the default logger',
+  PATH_TO_KEYV_DB: 'the path to the database file (e.g. sqlite://path/to/data.db)',
+  PATH_TO_GRAPHQL_SCHEMA: 'the path to the GraphQL schema (e.g. schema/schema.graphql)',
+}
 
-  return { err: new Error(`expected \`${key}\` {${is.getType(value)}} to be a {string} of 256 or more bits (32 or more utf8 chars)`) }
-})
-
-const envvarsBlueprint = {
-  NODE_ENV: /^(local|test|development|production)$/,
-  NODE_ENV_OPTIONS: required('any').from(() => ({
+const BaseEnv = z.object({
+  NODE_ENV: z.enum(['local', 'development', 'test', 'production'])
+    .describe(DESC.NODE_ENV),
+  NODE_ENV_OPTIONS: z.object({
+    LOCAL: z.string().min(3).trim(),
+    TEST: z.string().min(3).trim(),
+    DEV: z.string().min(3).trim(),
+    PROD: z.string().min(3).trim(),
+  }).default({
     LOCAL: 'local',
     TEST: 'test',
     DEV: 'development',
     PROD: 'production',
-  })),
-  /**
-   * This ENVVAR is used to turn dev features on and off, such as
-   * GraphiQL, and the Content-Security-Policies that go with that
-   */
-  ALLOW_DEV_CONFIGURATIONS: optional('boolean').from(({ output }) => [
-    output.NODE_ENV_OPTIONS.LOCAL,
-    output.NODE_ENV_OPTIONS.DEV,
-  ].includes(output.NODE_ENV)),
-  ENFORCE_HTTPS: optional('boolean').from(({ output }) => !output.ALLOW_DEV_CONFIGURATIONS),
-  APP_VERSION: required('string').from(({ value, input }) => value || input.npm_package_version),
-  PORT: optional('string').withDefault('3000'),
-  ROUTER_PREFIX: optional('string').withDefault(''),
-  /**
-   * When the app is in proxy, we have to set a flag in koa so it will set
-   * secure cookies (required IFF the app is in a proxy). Also turns on koa
-   * support for translating "X-Forwarded-For" headers to the consumer's
-   * IP Address for logging.
-   */
-  APP_IS_IN_PROXY: required('boolean').from(({ output }) =>
-    !!(typeof output.ROUTER_PREFIX && output.ROUTER_PREFIX.length)),
-  WEB_APP_ORIGIN: required('string').from(({ value, output }) => {
-    if (is.string(value)) {
-      return value
-    }
-
-    switch (output.NODE_ENV) {
-      case output.NODE_ENV_OPTIONS.PROD:
-        return 'http://localhost:3001'
-      case output.NODE_ENV_OPTIONS.DEV:
-        return 'http://localhost:3001'
-      case output.NODE_ENV_OPTIONS.TEST:
-        return 'http://localhost:3001'
-      default: // local
-        return 'http://localhost:3001'
-    }
-  }),
-  CORS_ORIGIN: optional('string').from(({ value, output }) => is.string(value) ? value : output.WEB_APP_ORIGIN),
-  JWT_COOKIE_NAME: optional('string').withDefault('h95729s'),
-  JWT_SECRET: '256bitString',
-  /** JWT Expiry in the format that jsonwebtoken wants it: make sure it matches  JWT_EXPIRES_IN_MS! */
-  JWT_EXPIRES_IN: optional('string').withDefault('30d'),
-  /** Cookie Expiry in the millisecond format that koa's ctx.cookies.set wants it: make sure it matches JWT_EXPIRES_IN! */
-  JWT_EXPIRES_IN_MS: optional('number').withDefault(86400000 * 30 /* 30 days */),
-  LOG_WRITER: required(/^(ArrayWriter|ConsoleWriter|DevConsoleWriter|StdoutWriter)$/).from(({ value, output }) => {
-    if (is.string(value)) {
-      return value
-    }
-
-    switch (output.NODE_ENV) {
-      case output.NODE_ENV_OPTIONS.PROD:
-        return 'StdoutWriter'
-      case output.NODE_ENV_OPTIONS.DEV:
-        return 'StdoutWriter'
-      case output.NODE_ENV_OPTIONS.TEST:
-        return 'ArrayWriter'
-      default: // local
-        return 'DevConsoleWriter'
-    }
-  }),
-  LOG_FORMATTER: required(/^(BlockFormatter|BunyanFormatter|JsonFormatter|PassThroughFormatter|StringFormatter|SquashFormatter)$/)
-    .from(({ value, output }) => {
-      if (is.string(value)) {
+  }).describe(DESC.NODE_ENV_OPTIONS),
+  PORT: z.union([z.number(), z.string()]).default(3001)
+    .pipe(z.coerce.number().int().gte(3000))
+    .describe(DESC.PORT),
+  ROUTER_PREFIX: z.string().startsWith('/').trim().optional().describe(DESC.ROUTER_PREFIX),
+  WEB_APP_ORIGIN: z.string().url().trim().default('http://localhost:3001').describe(DESC.WEB_APP_ORIGIN),
+  JWT_COOKIE_NAME: z.string().trim().default('h95729s').describe(DESC.JWT_COOKIE_NAME),
+  JWT_SECRET: z.string().min(32).trim().describe(DESC.JWT_SECRET),
+  JWT_EXPIRES_IN: z.string().min(2).trim().default('30d').describe(DESC.JWT_EXPIRES_IN),
+  JWT_EXPIRES_IN_MS: z.number().int().positive().default(86400000 * 30 /* 30 days */).describe(DESC.JWT_EXPIRES_IN_MS),
+  PATH_TO_KEYV_DB: z.string().trim().describe(DESC.PATH_TO_KEYV_DB),
+  PATH_TO_GRAPHQL_SCHEMA: z.string().trim().describe(DESC.PATH_TO_GRAPHQL_SCHEMA),
+  LOG_WRITER: z.enum([
+    'ArrayWriter',
+    'ConsoleWriter',
+    'DevConsoleWriter',
+    'StdoutWriter',
+  ]).default('StdoutWriter')
+    .describe(DESC.LOG_WRITER),
+  LOG_FORMATTER: z.enum([
+    'BlockFormatter',
+    'BunyanFormatter',
+    'JsonFormatter',
+    'PassThroughFormatter',
+    'StringFormatter',
+    'SquashFormatter',
+  ]).default('SquashFormatter')
+    .describe(DESC.LOG_FORMATTER),
+  LOG_LISTENERS: z.union([z.string(), z.string().array()])
+    .default(['info', 'warn', 'error', 'fatal'])
+    .transform((value) => {
+      if (Array.isArray(value)) {
         return value
+      } else if (typeof value === 'string') {
+        return value.split(',').map((/** @type {string} */ s) => s.trim())
+      } else {
+        throw new Error(`Expected {${typeof value}} to be a {string} or {string[]}`)
       }
+    })
+    .pipe(z.coerce.string().array())
+    .describe(DESC.LOG_LISTENERS),
+})
 
-      switch (output.NODE_ENV) {
-        case output.NODE_ENV_OPTIONS.PROD:
-          return 'SquashFormatter'
-        case output.NODE_ENV_OPTIONS.DEV:
-          return 'SquashFormatter'
-        case output.NODE_ENV_OPTIONS.TEST:
-          return 'PassThroughFormatter'
-        default: // local
-          return 'BlockFormatter'
-      }
-    }),
-  LOG_LISTENERS: required('string[]').from(({ value, output }) => {
-    if (is.string(value)) {
-      return value.split(',').map((/** @type {string} */ s) => s.trim())
-    }
+const CalculatedEnvvars = z.object({
+  APP_VERSION: z.string().min(2).trim().optional().describe(DESC.APP_VERSION),
+  ALLOW_DEV_CONFIGURATIONS: z.boolean().optional().describe(DESC.ALLOW_DEV_CONFIGURATIONS),
+  ENFORCE_HTTPS: z.boolean().optional().describe(DESC.ENFORCE_HTTPS),
+  APP_IS_IN_PROXY: z.boolean().optional().describe(DESC.APP_IS_IN_PROXY),
+  CORS_ORIGIN: z.string().url().trim().optional().describe(DESC.CORS_ORIGIN),
+})
 
-    /**
-     * Example listeners:
-     * // metrics
-     * 'count',
-     * 'error_count',
-     * 'gauge_increase',
-     * 'gauge_decrease',
-     * 'latency',
-     * // audit trail
-     * // (i.e. events that should be auditable, such as access to, or modification of PII)
-     * 'audit_info',
-     * 'audit_warn',
-     * // only acceptable on local dev
-     * // (i.e. debug secrets without risking accidental logging in master)
-     * 'local',
-     * 'todo',
-     * // intended for programmatic testing
-     * // (i.e. debug private/encapsulated code, sequences, etc.)
-     * 'test',
-     * // standard logging fare (events)
-     * 'trace',
-     * 'debug',
-     * 'info',
-     * 'warn',
-     * 'error',
-     * 'fatal',
-     */
+export const EnvSchema = BaseEnv.merge(CalculatedEnvvars).transform((value) => {
+  value.APP_VERSION = value.APP_VERSION || process?.env?.npm_package_version
+  value.APP_IS_IN_PROXY = !!(value.ROUTER_PREFIX && value.ROUTER_PREFIX.length)
+  value.CORS_ORIGIN = value.CORS_ORIGIN || value.WEB_APP_ORIGIN
+  value.ALLOW_DEV_CONFIGURATIONS = [
+    value.NODE_ENV_OPTIONS.LOCAL,
+    value.NODE_ENV_OPTIONS.DEV,
+  ].includes(value.NODE_ENV)
+  value.ENFORCE_HTTPS = !value.ALLOW_DEV_CONFIGURATIONS
 
-    switch (output.NODE_ENV) {
-      case output.NODE_ENV_OPTIONS.PROD:
-        return ['info', 'warn', 'error', 'fatal']
-      case output.NODE_ENV_OPTIONS.DEV:
-        return ['debug', 'info', 'warn', 'error', 'fatal']
-      case output.NODE_ENV_OPTIONS.TEST:
-        return ['test', 'local', 'todo', 'trace', 'debug', 'info', 'warn', 'error', 'fatal']
-      default: // local
-        // return ['*'] // all events
-        return ['local', 'todo', 'trace', 'debug', 'info', 'warn', 'error', 'fatal']
-    }
-  }),
-  PATH_TO_KEYV_DB: 'string',
-  PATH_TO_GRAPHQL_SCHEMA: 'string',
-}
-
-registerBlueprint('ENVVARS', envvarsBlueprint)
+  return value
+})
 
 /** @type {_ENVVARS} */
-const _ENVVARS = immutable('ENVVARS', envvarsBlueprint)
+const _ENVVARS = immutable('ENVVARS', EnvSchema)
 
 /** @type {ENVVARS} */
 export default class Envvars extends _ENVVARS {
-  static blueprint = envvarsBlueprint
+  static schema = EnvSchema
 }
