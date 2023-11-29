a0617f9c0c4a153969353f55da8c96f7a48d089c
Wendy-wyt <wuyuting0101@gmail.com>
2023-11-29 14:42:12 -0500

    ft io

--- src/domains/chat/src/io/send-user-interaction.ts
+++ src/domains/chat/src/io/send-user-interaction.ts
@@ -1,7 +1,22 @@
 // src/domains/chat/src/io/send-user-interaction.ts
 
-export async function sendUserInteraction(userInput: string): Promise<string> {
+import { z } from 'zod'
+import { env } from '@lib/env'
+import { join, useFetch } from '@lib/fetch'
+import { addBotMessage, useChatStore } from '../state/user-interaction-store'
+
+
+export async function sendUserInteraction(userInput: string){
   // Here we should send the user input to your chatbot (AI) service and get a response.
   // For now, I mocked response.  we have to change this with the response of AI  service
-  return `You said, ${userInput} please confirm?: `;
+
+  const [
+    error,
+    loading,
+    fetchStatus
+  ] = useFetch<String>(
+    join(env.PUBLIC_API_ORIGIN, '/api/answer_question'),
+    z.string(),
+    (response) => { addBotMessage(response.toString()) },
+  )
 }
